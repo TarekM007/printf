02ef395b8aa3c761596b15065a446e04b51be611
SalM1a <salma.abodokhan@gmail.com>
2023-10-16 13:01:40 -0700

    printf_project

---
+++ README.md
@@ -0,0 +1,1 @@
+Description of a custom printf function.
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_int(int num);
+
+
+#endif /* MAIN_H */
