7c41775ae181549651c5e7a8dcfd127c45d1021b
SalM1a <salma.abodokhan@gmail.com>
2023-10-17 10:14:52 -0700

    update2

--- _print.c
+++ _print.c
@@ -6,35 +6,45 @@
  */
 int _printf(const char *format, ...)
 {
-int i = 0, char_counter = 0;
+char *str;
+int i, count = 0;
 va_list args;
-
-if (format == NULL)
-return (-1);
 va_start(args, format);
 while (*format)
 {
-if (*format != '%')
+if (*format == '%')
 {
-write(1, format, 1);
-char_counter++; }
-else
+switch (*++format)
 {
-format++;
-if (*format == '\0')
+case 'c':
+_putchar(va_arg(args, int));
+count++;
 break;
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-write(1, &c, 1);
-char_counter++; }
-else if (*format == 's')
-{
-char *str = va_arg(args, char*);
-while (str[i] != '\0')
-i++;
-write(1, str, i);
-char_counter +=  i; }}
-format++; }
+case '%':
+_putchar('%');
+break;
+case 's':
+str = va_arg(args, char*);
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+count += str[i];
+break;
+case 'd': case 'i':
+print_int(va_arg(args, int));
+count++;
+break;
+default:
+_putchar('%');
+_putchar(*format);
+count++;
+break;
+}
+}
+else
+_putchar(*format);
+count++;
+format++;
+}
 va_end(args);
-return (char_counter); }
+return (count);
+}
