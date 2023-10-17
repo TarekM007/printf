7ade8ffbe0c5a0b25983c003c3eb996f85686a32
SalM1a <salma.abodokhan@gmail.com>
2023-10-17 15:20:09 -0700

    update8

---
+++ print_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * printf_string - a functions that prints a string.
+ * @st: argument as input.
+ * Return: the length of the string.
+ */
+int printf_string(va_list st)
+{
+char *s;
+int i, len;
+
+s = va_arg(st, char *);
+if (s == NULL)
+{
+s = "(null)";
+len = _strlen(s);
+for (i = 0; i < len; i++)
+_putchar(s[i]);
+return (len);
+}
+else
+{
+len = _strlen(s);
+for (i = 0; i < len; i++)
+_putchar(s[i]);
+return (len);
+}
+}
